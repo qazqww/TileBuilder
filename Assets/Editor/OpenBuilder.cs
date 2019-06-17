d67a0f23427a4f9be0ec15ff7abc3c0965660ec7
ktm-jg <raptarior@gmail.com>
2019-06-17 10:03:59 +0900

    Initial commit

---
+++ Assets/Editor/OpenBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class OpenBuilder
+{
+    [MenuItem("Window/OpenBuilder")]
+    public static void OpenMenu()
+    {
+
+    }
+}
